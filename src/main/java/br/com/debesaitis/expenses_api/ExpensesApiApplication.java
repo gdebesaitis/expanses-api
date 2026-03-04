1f003ed01725860a2a633af2bf09561b972155e0
gdebesaitis <guilhermedbdev@gmail.com>
2026-03-04 09:50:40 -0300

    start project

---
+++ src/main/java/br/com/debesaitis/expenses_api/ExpensesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.debesaitis.expenses_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/debesaitis/expenses_api/ExpensesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.debesaitis.expenses_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
